a27d1c949c239f7b2d7a072e1b3c88667d3296fc
Michael Zhao <mizh@protonmail.ch>
2021-08-23 10:01:32 +0000

    gulpfile.js: remove deprecated packages
    * removes lazy loading of gulp-load-plugins
    * completes migration of gulp-util to Gulp4

--- gulpfile.js
+++ gulpfile.js
@@ -2,47 +2,61 @@
 
 // For Gulp3 documentation see https://github.com/gulpjs/gulp/blob/v3.9.1/docs/API.md
 
+// Dependencies
 const { src, dest, series, parallel, task, watch } = require('gulp');
-const $ = require('gulp-load-plugins')({ lazy: true });
-const replace = require('gulp-replace');
-const del = require('del');
-const dev = require('yargs').argv.dev;
-var sass = require('gulp-sass')(require('sass'));
 var config = require('./gulp.config.js')();
+const colors = require('ansi-colors');
+const del = require('del');
+const log = require('fancy-log');
+const angularTemplateCache = require('gulp-angular-templatecache');
+const csso = require('gulp-csso');
+const filter = require('gulp-filter');
+const gulpif = require('gulp-if');
+const inject = require('gulp-inject');
+const minifyHTML = require('gulp-minify-html');
+const gulporder = require('gulp-order');
+const replace = require('gulp-replace');
+const rev = require('gulp-rev');
+const revReplace = require('gulp-rev-replace');
+var sass = require('gulp-sass')(require('sass'));
+const taskListing = require('gulp-task-listing');
+const useref = require('gulp-useref');
 
-task('help', $.taskListing); // Lists all public tasks
+const dev = require('yargs').argv.dev;
+
+task('help', taskListing); // Lists all public tasks
 
 /***** Helper Functions *****/
 
 function clean(path, done) {
-    log('Cleaning: ' + $.util.colors.blue(path));
+    log('Cleaning: ' + colors.blue(path));
     del(path, { force: true }).then(paths => done());
 }
 
-function log(msg) {
+function printLog(msg) { // our special way of printing log messages (this is probably unnecessary)
     if (typeof (msg) === 'object') {
         for (var item in msg) {
             if (msg.hasOwnProperty(item)) {
-                $.util.log($.util.colors.blue(msg[item]));
+                log(colors.blue(msg[item]));
             }
         }
     } else {
-        $.util.log($.util.colors.blue(msg));
+        log(colors.blue(msg));
     }
 }
 
-function inject(source, label, order) {
+function injectAtLabels(source, label, order) { // Injects the source file/s into the target file/s at defined placeholder labels
     var options = { addRootSlash: false };
     if (label) {
         options.name = 'inject:' + label;
     }
 
-    return $.inject(orderSrc(source, order), options).on('error', log);
+    return inject(orderSrc(source, order), options).on('error', log);
 }
 
 function orderSrc(source, order) {
     return src(source)
-        .pipe($.if(order, $.order(order)));
+        .pipe(gulpif(order, gulporder(order)));
 }
 
 /***** Helper Functions: Cleaning *****/
@@ -65,12 +79,12 @@ function cleanBuild(done) {
 /***** Helper Tasks: Template Building *****/
 
 task('template-cache', series(cleanCode, function () {
-    log('Creating an AngularJS $templateCache');
+    printLog('Creating an AngularJS $templateCache');
 
     return src(config.htmltemplates)
-        .pipe($.minifyHtml({ empty: true }))
-        .pipe($.order(['*']))
-        .pipe($.angularTemplatecache(
+        .pipe(minifyHTML({ empty: true }))
+        .pipe(gulporder(['*']))
+        .pipe(angularTemplateCache(
             config.templateCache.file,
             config.templateCache.options
         ))
@@ -78,7 +92,7 @@ task('template-cache', series(cleanCode, function () {
 }));
 
 task('remove-template', function () {
-    log('Removing template');
+    printLog('Removing template');
 
     return src(config.index)
         .pipe(replace(
@@ -91,7 +105,7 @@ task('remove-template', function () {
 /***** Helper Tasks: Sass -> CSS ******/
 
 task('compile-sass', series(cleanStyles, function () {
-    log('Compiling SASS --> CSS');
+    printLog('Compiling SASS --> CSS');
 
     return src(config.sassDir)
         .pipe(sass())
@@ -113,16 +127,16 @@ if ( dev ) {
 }
 
 task('inject', series(injectDependencies, function () { // first run compile-sass, then template-cache, then finally run inject
-    log('Wire up css and js into the html, after files are ready');
+    printLog('Wire up css and js into the html, after files are ready');
 
     var result = src(config.index)
-        .pipe(inject(config.cssFile))
-        .pipe(inject(config.js, '', config.jsOrder));
+        .pipe(injectAtLabels(config.cssFile))
+        .pipe(injectAtLabels(config.js, '', config.jsOrder));
 
     // Only put in the template cache if we're not in dev mode
     if ( !dev ) {
         var templateCache = config.temp + config.templateCache.file;
-        result = result.pipe(inject(templateCache, 'templates'));
+        result = result.pipe(injectAtLabels(templateCache, 'templates'));
     }
 
     result = result.pipe(dest(config.root));
@@ -166,22 +180,19 @@ task('copy-resources', series(copyThirdParty, copyJSON, copyImages, copyManifest
 /**** Optimisation Task *****/
 
 task('optimize', series('inject', cleanBuild, 'copy-resources', function () {
-    var cssFilter = $.filter('./styles/*.css', {restore: true});
+    var cssFilter = filter('./styles/*.css', {restore: true});
     // TODO: JS filter temporarily disabled until the dependency injection issue is resolved
-    // var jsAppFilter = $.filter('**/' + config.optimized.app, {restore: true});
+    // var jsAppFilter = filter('**/' + config.optimized.app, {restore: true});
     // comment: Dependency injection has been resolved, uncomment this.
-    var indexHtmlFilter = $.filter(['**/*', '!**/index.html'], { restore: true });
+    var indexHtmlFilter = filter(['**/*', '!**/index.html'], { restore: true });
     return src(config.index)
-        .pipe($.useref())
+        .pipe(useref())
         .pipe(cssFilter)
-            .pipe($.csso())
+            .pipe(csso())
             .pipe(cssFilter.restore)
-        // .pipe(jsAppFilter)
-        //     .pipe($.uglify())
-        //     .pipe(jsAppFilter.restore)
         .pipe(indexHtmlFilter)
-            .pipe($.rev())
+            .pipe(rev())
             .pipe(indexHtmlFilter.restore)
-        .pipe($.revReplace())
+        .pipe(revReplace())
         .pipe(dest(config.build));
 }));
